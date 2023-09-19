4238915094f87d15ebb8f6d7ddbb7aa2437653d3
VincenzoDePetris <vince02@live.it>
2023-09-19 12:29:01 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import axios from "axios";
+import { store } from "./data/store";
+
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppHeader.vue";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+  components: { AppHeader, AppMain, AppFooter },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
+
+<style lang="scss">
+@use "./assets/general.scss" as *;
+</style>
